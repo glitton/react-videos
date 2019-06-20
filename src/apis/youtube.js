21f0493217106c2bd475107bed7dbfd42cc25566
Generosa Litton <glitton@newrelic.com>
2019-06-19 22:53:28 -0700

    added axios

--- src/apis/youtube.js
+++ src/apis/youtube.js
@@ -1,1 +1,12 @@
-const KEY = 'AIzaSyDOoZduHfKeVpIjQGIhA3nue1EnUw-3mwk';
+import axios from "axios";
+
+const KEY = "AIzaSyDOoZduHfKeVpIjQGIhA3nue1EnUw-3mwk";
+
+export default axios.create({
+  baseURL: "https://www.googleapis.com/youtube/v3",
+  params: {
+    part: "snippet",
+    maxResults: 5,
+    key: KEY
+  }
+});
