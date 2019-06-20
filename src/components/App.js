f956bf28f135620e960a304997b7949c3eb31ae5
Generosa Litton <glitton@newrelic.com>
2019-06-19 21:43:27 -0700

    added components App SearchBar

---
+++ src/components/App.js
@@ -0,0 +1,15 @@
+/* eslint-disable react/prefer-stateless-function */
+import React from 'react';
+import SearchBar from './SearchBar';
+
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <SearchBar />
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/components/SearchBar.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+// eslint-disable-next-line react/prefer-stateless-function
+class SearchBar extends React.Component {
+  render() {
+    return <div>SearchBar</div>;
+  }
+}
+
+export default SearchBar;
