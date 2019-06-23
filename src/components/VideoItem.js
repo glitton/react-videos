e3f36c70655aec459fe36f01919d04e9ce49d7c1
Generosa Litton <glitton@newrelic.com>
2019-06-22 23:22:03 -0700

    desctructuring in video item

--- src/components/VideoItem.js
+++ src/components/VideoItem.js
@@ -1,7 +1,14 @@
 import React from "react";
 
-const VideoItem = props => {
-  return <div>Video Item</div>;
+const VideoItem = ({ video }) => {
+  // console.log({ video });
+  const { thumbnails, description, title } = video.snippet;
+  return (
+    <div>
+      <img src={thumbnails.medium.url} alt={description} />
+      {title}
+    </div>
+  );
 };
 
 export default VideoItem;
--- src/components/VideoList.js
+++ src/components/VideoList.js
@@ -4,7 +4,7 @@ import VideoItem from "./VideoItem";
 // instead of props.videos do {videos}
 const VideoList = ({ videos }) => {
   const renderedList = videos.map(video => {
-    return <VideoItem />;
+    return <VideoItem video={video} />;
   });
   return <div>{renderedList}</div>;
 };
