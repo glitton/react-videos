410566e5e3be7f1077820ce8a5465f7647e649a6
Generosa Litton <glitton@newrelic.com>
2019-06-29 21:01:55 -0700

    fixed warnings

--- src/components/VideoItem.js
+++ src/components/VideoItem.js
@@ -4,10 +4,10 @@ import React from "react";
 // just video like {video}
 const VideoItem = ({ video, onVideoSelect }) => {
   // console.log({ video });
-  const { thumbnails, description, title } = video.snippet;
+  const { thumbnails, title } = video.snippet;
   return (
     <div onClick={() => onVideoSelect(video)} className="video-item item">
-      <img className="ui image" src={thumbnails.medium.url} alt={description} />
+      <img className="ui image" src={thumbnails.medium.url} alt={title} />
       <div className="content">
         <div className="header">{title}</div>
       </div>
